66ffb6a8f232b5e07022916500ab18a5d15be87d
falakjain98 <jain.falak@hotmail.com>
2022-07-15 06:13:55 +0000

    week 2 homework data_ingestion_gcs_dag created, dag not functional

--- week_2_data_ingestion/airflow_homework/dags/data_ingestion_gcs_dag.py
+++ week_2_data_ingestion/airflow_homework/dags/data_ingestion_gcs_dag.py
@@ -1,33 +1,27 @@
 import os
 import logging
 
+from datetime import datetime
+
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 from google.cloud import storage
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
+
 import pyarrow.csv as pv
 import pyarrow.parquet as pq
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
-dataset_file = "yellow_tripdata_2021-01.parquet"
-dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
-path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-parquet_file = dataset_file.replace('.csv', '.parquet')
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+URL_PREFIX = 'https://d37ci6vzurychx.cloudfront.net/trip-data'
 
-
-# def format_to_parquet(src_file):
-#     if not src_file.endswith('.csv'):
-#         logging.error("Can only accept source files in CSV format, for the moment")
-#         return
-#     table = pv.read_csv(src_file)
-#     pq.write_table(table, src_file.replace('.csv', '.parquet'))
-
+YELLOW_TAXI_URL_TEMPLATE = URL_PREFIX + '/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+YELLOW_TAXI_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+YELLOW_TAXI_GCS_PATH_TEMPLATE = "raw/yellow_tripdata/{{ execution_date.strftime(\'%Y\') }}/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet"
 
 # NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
 def upload_to_gcs(bucket, object_name, local_file):
@@ -53,58 +47,56 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 default_args = {
     "owner": "airflow",
-    "start_date": days_ago(1),
+    #"start_date": days_ago(1),
     "depends_on_past": False,
     "retries": 1,
 }
 
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
-with DAG(
-    dag_id="data_ingestion_gcs_dag",
-    schedule_interval="@daily",
+def download_parquetize_upload_dag(
+    dag,
+    url_template,
+    local_parquet_path_template,
+    gcs_path_template
+):
+    with dag:
+        download_dataset_task = BashOperator(
+            task_id="download_dataset_task",
+            bash_command=f"curl -sSL {url_template} > {local_parquet_path_template}"
+        )
+
+        # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
+        local_to_gcs_task = PythonOperator(
+            task_id="local_to_gcs_task",
+            python_callable=upload_to_gcs,
+            op_kwargs={
+                "bucket": BUCKET,
+                "object_name": gcs_path_template,
+                "local_file": local_parquet_path_template,
+            },
+        )
+
+        rm_task = BashOperator(
+            task_id = "rm_task"
+            bash_command = f"rm {local_parquet_path_template}"
+        )
+
+        download_dataset_task >> local_to_gcs_task >> rm_task
+
+yellow_taxi_data_dag = DAG(
+    dag_id="yellow_taxi_data_v2",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(2019, 1, 1),
+    end_date=datetime(2020,12,31)
     default_args=default_args,
-    catchup=False,
-    max_active_runs=1,
+    catchup=True,
+    max_active_runs=3,
     tags=['dtc-de'],
-) as dag:
+)
 
-    download_dataset_task = BashOperator(
-        task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
-    )
-
-    # format_to_parquet_task = PythonOperator(
-    #     task_id="format_to_parquet_task",
-    #     python_callable=format_to_parquet,
-    #     op_kwargs={
-    #         "src_file": f"{path_to_local_home}/{dataset_file}",
-    #     },
-    # )
-
-    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
-    local_to_gcs_task = PythonOperator(
-        task_id="local_to_gcs_task",
-        python_callable=upload_to_gcs,
-        op_kwargs={
-            "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
-        },
-    )
-
-    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id="bigquery_external_table_task",
-        table_resource={
-            "tableReference": {
-                "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_table",
-            },
-            "externalDataConfiguration": {
-                "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
-            },
-        },
-    )
-
-    download_dataset_task >> local_to_gcs_task >> bigquery_external_table_task
+donwload_parquetize_upload_dag(
+    dag=yellow_taxi_data_dag,
+    url_template=YELLOW_TAXI_URL_TEMPLATE,
+    local_parquet_path_template=YELLOW_TAXI_PARQUET_FILE_TEMPLATE,
+    gcs_path_template=YELLOW_TAXI_GCS_PATH_TEMPLATE
+)
