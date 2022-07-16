8f92f81997049b43ac4ac60b5f07cd596e812941
falakjain98 <jain.falak@hotmail.com>
2022-07-16 19:36:41 +0000

    Include DAG for FHV and Zone Data

--- week_2_data_ingestion/homework/airflow/dags/data_ingestion_gcs_dag.py
+++ week_2_data_ingestion/homework/airflow/dags/data_ingestion_gcs_dag.py
@@ -17,30 +17,28 @@ PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
+# URL prefix for yellow and fhv taxi data
 URL_PREFIX = 'https://d37ci6vzurychx.cloudfront.net/trip-data'
 
+# Arguments for yellow taxi dag
 YELLOW_TAXI_URL_TEMPLATE = URL_PREFIX + '/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
 YELLOW_TAXI_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
 YELLOW_TAXI_GCS_PATH_TEMPLATE = "raw/yellow_tripdata/{{ execution_date.strftime(\'%Y\') }}/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet"
 
+# Arguments for fhv taxi dag
+FHV_URL_TEMPLATE = URL_PREFIX + '/fhv_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+FHV_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + '/fhv_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+FHV_GCS_PATH_TEMPLATE = "raw/fhv_tripdata/{{ execution_date.strftime(\'%Y\') }}/fhv_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet"
+
+# Arguments for zone table
+ZONES_URL_TEMPLATE = 'https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.parquet'
+ZONES_PARQUET_FILE_TEMPLATE = AIRFLOW_HOME + "/taxi_zone_lookup.parquet"
+ZONES_GCS_PATH_TEMPLATE = "raw/taxi_zone/taxi_zone_lookup.parquet"
+
 # NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
 def upload_to_gcs(bucket, object_name, local_file):
-    """
-    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
-    :param bucket: GCS bucket name
-    :param object_name: target path & file-name
-    :param local_file: source path & file-name
-    :return:
-    """
-    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
-    # (Ref: https://github.com/googleapis/python-storage/issues/74)
-    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
-    # End of Workaround
-
     client = storage.Client()
     bucket = client.bucket(bucket)
-
     blob = bucket.blob(object_name)
     blob.upload_from_filename(local_file)
 
@@ -62,7 +60,8 @@ def download_parquetize_upload_dag(
     with dag:
         download_dataset_task = BashOperator(
             task_id="download_dataset_task",
-            bash_command=f"curl -sSL {url_template} > {local_parquet_path_template}"
+            # -f flag causes url to fail if the url does not exist, we don't want a green box if something fails
+            bash_command=f"curl -sSLf {url_template} > {local_parquet_path_template}"
         )
 
         # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
@@ -76,6 +75,7 @@ def download_parquetize_upload_dag(
             },
         )
 
+        # Remove file for local folder to reduce storage
         rm_task = BashOperator(
             task_id = "rm_task",
             bash_command = f"rm {local_parquet_path_template}"
@@ -83,8 +83,9 @@ def download_parquetize_upload_dag(
 
         download_dataset_task >> local_to_gcs_task >> rm_task
 
+# Run dag for yellow taxi
 yellow_taxi_data_dag = DAG(
-    dag_id="yellow_taxi_data_v2",
+    dag_id="yellow_taxi_data",
     schedule_interval="0 6 2 * *",
     start_date=datetime(2019, 1, 1),
     end_date=datetime(2020,12,31),
@@ -99,4 +100,41 @@ download_parquetize_upload_dag(
     url_template=YELLOW_TAXI_URL_TEMPLATE,
     local_parquet_path_template=YELLOW_TAXI_PARQUET_FILE_TEMPLATE,
     gcs_path_template=YELLOW_TAXI_GCS_PATH_TEMPLATE
+)
+
+# Run dag for fhv taxi
+fhv_taxi_data_dag = DAG(
+    dag_id="fhv_taxi_data",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(2019, 1, 1),
+    end_date=datetime(2019,12,31),
+    default_args=default_args,
+    catchup=True,
+    max_active_runs=3,
+    tags=['dtc-de'],
+)
+
+download_parquetize_upload_dag(
+    dag=fhv_taxi_data_dag,
+    url_template=FHV_URL_TEMPLATE,
+    local_parquet_path_template=FHV_PARQUET_FILE_TEMPLATE,
+    gcs_path_template=FHV_GCS_PATH_TEMPLATE
+)
+
+# Run dag for zones file
+zones_data_dag = DAG(
+    dag_id="zones_data",
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    default_args=default_args,
+    catchup=True,
+    max_active_runs=3,
+    tags=['dtc-de'],
+)
+
+download_parquetize_upload_dag(
+    dag=zones_data_dag,
+    url_template=ZONES_URL_TEMPLATE,
+    local_parquet_path_template=ZONES_PARQUET_FILE_TEMPLATE,
+    gcs_path_template=ZONES_GCS_PATH_TEMPLATE
 )
