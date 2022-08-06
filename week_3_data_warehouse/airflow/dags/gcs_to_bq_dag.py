687f61afcc2e3598ce19eaafb930a05ecdc38b82
falakjain98 <jain.falak@hotmail.com>
2022-08-06 15:30:52 +0000

    Update gcs to bq dag to green taxi

--- week_3_data_warehouse/airflow/dags/gcs_to_bq_dag.py
+++ week_3_data_warehouse/airflow/dags/gcs_to_bq_dag.py
@@ -29,66 +29,8 @@ default_args = {
 }
 
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
-with DAG(
-    dag_id="gcs_2_bq_dag_yellow",
-    schedule_interval="@daily",
-    default_args=default_args,
-    catchup=False,
-    max_active_runs=1,
-    tags=['dtc-de'],
-) as dag:
-
-    for colour, ds_col in YELLOW_COLOUR_RANGE.items():
-        # GCS to GCS task
-        move_files_gcs_task = GCSToGCSOperator(
-            task_id=f'move_{colour}_{DATASET}_files_task',
-            source_bucket=BUCKET,
-            source_object=f'{INPUT_PART}/{colour}_{DATASET}*.{INPUT_FILETYPE}',
-            destination_bucket=BUCKET,
-            destination_object=f'{colour}/{colour}_{DATASET}',
-            move_object=True
-        )
-
-        # Creating BigQuery External Table
-        bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-            task_id=f"bq_{colour}_{DATASET}_external_table_task",
-            table_resource={
-                "tableReference": {
-                    "projectId": PROJECT_ID,
-                    "datasetId": BIGQUERY_DATASET,
-                    "tableId": f"{colour}_{DATASET}_external_table",
-                },
-                "externalDataConfiguration": {
-                    "autodetect": "True",
-                    "sourceFormat": f"{INPUT_FILETYPE.upper()}",
-                    "sourceUris": [f"gs://{BUCKET}/{colour}/*"],
-                },
-            },
-        )
-
-        CREATE_BQ_TBL_QUERY = (
-            f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{colour}_{DATASET} \
-            PARTITION BY DATE({ds_col}) \
-            AS \
-            SELECT * REPLACE(NULL AS airport_fee) FROM {BIGQUERY_DATASET}.{colour}_{DATASET}_external_table;"
-        )
-
-        # Create a partitioned table from external table, performs within BQ query
-        bq_create_partitioned_table_job = BigQueryInsertJobOperator(
-            task_id=f"bq_create_{colour}_{DATASET}_partitioned_table_task",
-            configuration={
-                "query": {
-                    "query": CREATE_BQ_TBL_QUERY,
-                    "useLegacySql": False,
-                }
-            }
-        )
-
-        move_files_gcs_task >> bigquery_external_table_task >> bq_create_partitioned_table_job
-
-# # NOTE: DAG declaration - using a Context Manager (an implicit way)
 # with DAG(
-#     dag_id="gcs_2_bq_dag_green",
+#     dag_id="gcs_2_bq_dag_yellow",
 #     schedule_interval="@daily",
 #     default_args=default_args,
 #     catchup=False,
@@ -96,7 +38,7 @@ with DAG(
 #     tags=['dtc-de'],
 # ) as dag:
 
-#     for colour, ds_col in GREEN_COLOUR_RANGE.items():
+#     for colour, ds_col in YELLOW_COLOUR_RANGE.items():
 #         # GCS to GCS task
 #         move_files_gcs_task = GCSToGCSOperator(
 #             task_id=f'move_{colour}_{DATASET}_files_task',
@@ -128,7 +70,7 @@ with DAG(
 #             f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{colour}_{DATASET} \
 #             PARTITION BY DATE({ds_col}) \
 #             AS \
-#             SELECT * REPLACE(NULL as ehail_fee) FROM {BIGQUERY_DATASET}.{colour}_{DATASET}_external_table;"
+#             SELECT * REPLACE(NULL AS airport_fee) FROM {BIGQUERY_DATASET}.{colour}_{DATASET}_external_table;"
 #         )
 
 #         # Create a partitioned table from external table, performs within BQ query
@@ -143,3 +85,61 @@ with DAG(
 #         )
 
 #         move_files_gcs_task >> bigquery_external_table_task >> bq_create_partitioned_table_job
+
+# # NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="gcs_2_bq_dag_green",
+    schedule_interval="@daily",
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=1,
+    tags=['dtc-de'],
+) as dag:
+
+    for colour, ds_col in GREEN_COLOUR_RANGE.items():
+        # GCS to GCS task
+        move_files_gcs_task = GCSToGCSOperator(
+            task_id=f'move_{colour}_{DATASET}_files_task',
+            source_bucket=BUCKET,
+            source_object=f'{INPUT_PART}/{colour}_{DATASET}*.{INPUT_FILETYPE}',
+            destination_bucket=BUCKET,
+            destination_object=f'{colour}/{colour}_{DATASET}',
+            move_object=True
+        )
+
+        # Creating BigQuery External Table
+        bigquery_external_table_task = BigQueryCreateExternalTableOperator(
+            task_id=f"bq_{colour}_{DATASET}_external_table_task",
+            table_resource={
+                "tableReference": {
+                    "projectId": PROJECT_ID,
+                    "datasetId": BIGQUERY_DATASET,
+                    "tableId": f"{colour}_{DATASET}_external_table",
+                },
+                "externalDataConfiguration": {
+                    "autodetect": "True",
+                    "sourceFormat": f"{INPUT_FILETYPE.upper()}",
+                    "sourceUris": [f"gs://{BUCKET}/{colour}/*"],
+                },
+            },
+        )
+
+        CREATE_BQ_TBL_QUERY = (
+            f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{colour}_{DATASET} \
+            PARTITION BY DATE({ds_col}) \
+            AS \
+            SELECT * REPLACE(NULL as ehail_fee) FROM {BIGQUERY_DATASET}.{colour}_{DATASET}_external_table;"
+        )
+
+        # Create a partitioned table from external table, performs within BQ query
+        bq_create_partitioned_table_job = BigQueryInsertJobOperator(
+            task_id=f"bq_create_{colour}_{DATASET}_partitioned_table_task",
+            configuration={
+                "query": {
+                    "query": CREATE_BQ_TBL_QUERY,
+                    "useLegacySql": False,
+                }
+            }
+        )
+
+        move_files_gcs_task >> bigquery_external_table_task >> bq_create_partitioned_table_job
